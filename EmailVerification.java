5e4d9bda0d45df33ca124210495a3ffdc3aefcc2
JayapreethaJ <jayaraman4preetha@gmail.com>
2025-12-12 22:58:26 +0530

    email verification

---
+++ EmailVerification.java
@@ -0,0 +1,23 @@
+public class EmailVerification{
+     String firstName,lastName,email;
+        int age;
+        
+        public EmailVerification(String firstName,String lastName, String email){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+        }
+        public EmailVerification(int age){
+            this.age = age;
+        }
+    public static void main(String[] args) {
+        EmailVerification name = new EmailVerification("jaya","preetha","jayaraman4preetha@gmail.com");
+        EmailVerification age = new EmailVerification(23);
+
+        System.out.println("Name:" + name.firstName + name.lastName + name.email);
+        System.out.println("age:" + age.age);
+
+       
+    }
+}
+
